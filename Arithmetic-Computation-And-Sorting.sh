144f8d9c3b455b4189d75151ab45f606cc773585
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 10:47:00 +0530

    ADDED:Performe Third Computation

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -5,3 +5,4 @@ read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
 result1=`echo "scale=2; $a+$b*$c" | bc`
 result2=`echo "scale=2; $c+$a/$b" | bc`
+result3=`echo "scale=2; $c+$a/$b" | bc`
