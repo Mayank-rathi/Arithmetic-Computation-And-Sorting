e606b7592678e342ec274497964adcf0b7540a24
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 14:51:15 +0530

    ADDED:Sort Result In Descending

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -16,10 +16,25 @@ DictionaryResult[result2]=$result2
 DictionaryResult[result3]=$result3
 DictionaryResult[result4]=$result4
 
-
 for((i=1; i<=${#DictionaryResult[@]};i++))
 do
 	array[$count]=${DictionaryResult[result"$(( count+1))"]}
 	count=$(( count+1 ))
 done
 	echo ${array[@]}
+
+for((i=0;i<3;i++))
+do
+	for((j=i+1;j<4;j++))
+	do
+		if [[ ${array[i]%.*} -lt ${array[j]%.*} ]]
+		then
+				temp=${array[i]}
+				array[i]=${array[j]}
+				array[j]=$temp
+		fi
+	done
+done
+echo Descending order : ${array[@]}
+
+	
