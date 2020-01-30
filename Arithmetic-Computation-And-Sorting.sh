a979623e8525d3bc62fc2a280903e4747b04469a
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 10:22:24 +0530

    ADDED:Perform First Computation

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
-
 echo "Enter Three Inputs"
 read -p "Enter First Number: " a
 read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
+result=`echo "scale=2; $a+($b*$c)" | bc`
+
