0c0db79ce9f13fbf3370e3ac11763ebdeb336a99
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 10:56:34 +0530

    MODIFY AND ADDED:Change result2 Equation And Add Third Computation

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -4,4 +4,5 @@ read -p "Enter First Number: " a
 read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
 result1=`echo "scale=2; $a+$b*$c" | bc`
-result2=`echo "scale=2; $c+$a/$b" | bc`
+result2=`echo "scale=2; $a*$b+$c" | bc`
+result3=`echo "scale=2; $c+$a/$b" | bc`
