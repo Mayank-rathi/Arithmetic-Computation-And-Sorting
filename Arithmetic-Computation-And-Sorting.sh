e77bce3ff309cc826b58db94e8e1ac166dd425d2
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 11:09:58 +0530

    ADDED:Performe Fouth Computation

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -6,3 +6,4 @@ read -p "Enter Third Number: " c
 result1=`echo "scale=2; $a+$b*$c" | bc`
 result2=`echo "scale=2; $a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
+result4=`echo "scale=2; $a%$b+$c" | bc`
