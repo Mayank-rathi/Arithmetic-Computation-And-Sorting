6d909033c71f3ff5b4fb049e0dd384bd67c0db7c
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 10:09:13 +0530

    MODIFY AND ADDED:Remove Welcome Message And Add Three User Input

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -1,1 +1,6 @@
-echo "Welcome To Arithmetic Computation And Sorting"
+#!/bin/bash -x
+
+echo "Enter Three Inputs"
+read -p "Enter First Number: " a
+read -p "Enter Second Number: " b
+read -p "Enter Third Number: " c
