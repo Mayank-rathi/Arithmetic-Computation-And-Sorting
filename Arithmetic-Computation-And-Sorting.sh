8fbcbf68d395ef1a185a4dbe4067fbb6afb03a02
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 11:41:49 +0530

    ADDED:store The Result In Dicionary

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
+declare -A DictionaryResult
 echo "Enter Three Inputs"
 read -p "Enter First Number: " a
 read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
-result1=`echo "scale=2; $a+$b*$c" | bc`
-result2=`echo "scale=2; $a*$b+$c" | bc`
-result3=`echo "scale=2; $c+$a/$b" | bc`
-result4=`echo "scale=2; $a%$b+$c" | bc`
+
+result1=`echo "$a+$b*$c" | bc`
+result2=`echo "$a*$b+$c" | bc`
+result3=`echo "$c+$a/$b" | bc`
+result4=`echo "$a%$b+$c" | bc`
+
+DictionaryResult[result1]=$result1
+DictionaryResult[result2]=$result2
+DictionaryResult[result3]=$result3
+DictionaryResult[result4]=$result4
