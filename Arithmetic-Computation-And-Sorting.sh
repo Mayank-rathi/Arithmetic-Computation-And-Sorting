63b34bf3e0f515d5106f330f6406cd79169ac9b1
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-30 12:52:14 +0530

    ADDED:Read Value From Dictionary To Array For This use For Loop

--- Arithmetic-Computation-And-Sorting.sh
+++ Arithmetic-Computation-And-Sorting.sh
@@ -4,6 +4,7 @@ echo "Enter Three Inputs"
 read -p "Enter First Number: " a
 read -p "Enter Second Number: " b
 read -p "Enter Third Number: " c
+count=0;
 
 result1=`echo "$a+$b*$c" | bc`
 result2=`echo "$a*$b+$c" | bc`
@@ -14,3 +15,11 @@ DictionaryResult[result1]=$result1
 DictionaryResult[result2]=$result2
 DictionaryResult[result3]=$result3
 DictionaryResult[result4]=$result4
+
+
+for((i=1; i<=${#DictionaryResult[@]};i++))
+do
+	array[$count]=${DictionaryResult[result"$(( count+1))"]}
+	count=$(( count+1 ))
+done
+	echo ${array[@]}
